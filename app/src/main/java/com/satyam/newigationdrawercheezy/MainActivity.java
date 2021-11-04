07ae16f6cd08d4423bf6059244e01e5e0721afa3
Satyam Prajapati <satyam7355pra@gmail.com>
2021-11-04 14:23:09 +0530

    second commit by satyam

--- app/src/main/java/com/satyam/newigationdrawercheezy/MainActivity.java
+++ app/src/main/java/com/satyam/newigationdrawercheezy/MainActivity.java
@@ -1,22 +1,43 @@
 package com.satyam.newigationdrawercheezy;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     Toolbar toolbar;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupToolbar();
+        navigationView=findViewById(R.id.nevigation_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        Toast.makeText(MainActivity.this,"Home item Clicked ",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_android:
+                        Toast.makeText(MainActivity.this,"android item clicked",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });
     }
     private void setupToolbar(){
         drawerLayout=findViewById(R.id.drawer_layout);
